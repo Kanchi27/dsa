05f99ffd222e1fd5177c246415cf9183983a4769
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-02-19 23:01:18 +0530

    Add files via upload

---
+++ SortingAlgorithms/BubbleSort.js
@@ -0,0 +1,23 @@
+// Bubble Sort -> bubble smallest element to the top
+function swap(a,b,arr){
+    [arr[b], arr[a]] = [arr[a], arr[b]];
+  }
+  
+  function bubbleSort(arr){
+    for(let i=0;i<arr.length-1 ; i++){
+      let noSwap = true;
+      for(j=arr.length;j>i; j--){
+        console.log('i',i,'j',j)
+        if(arr[j] < arr[j-1]){
+          swap(j,j-1,arr);
+          noSwap = false;
+        }
+      }
+       if(noSwap) {
+          break;
+        }
+    }
+    return arr
+  }
+  
+  console.log(bubbleSort([-2,9, 33, 2, 45, 17, 8]))
