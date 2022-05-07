532c66a7895569e6c8f37df71d081e9188890722
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-05-07 13:16:58 +0530

    Update Anangram.js

--- Anangram.js
+++ Anangram.js
@@ -1,5 +1,22 @@
 // Given 2 strings, check whether they are anagrams of each other
 
+// anagram with one count array
+function checkAnagram1(str1,str2){
+  if(str1.length!=str2.length){
+    return false
+  }
+  let countArr=new Array(26).fill(0);
+  for(let i=0;i<str1.length;i++){
+    let idx1 = str1[i].charCodeAt() -97;
+    let idx2 = str2[i].charCodeAt() -97;
+    countArr[idx1] = countArr[idx1]+1 ;
+    countArr[idx2] = countArr[idx2]-1;
+  }
+
+  return countArr.every(count => count===0);
+}
+
+
 function checkAnagram(str1,str2){
   if(str1.length!=str2.length){
     return false
