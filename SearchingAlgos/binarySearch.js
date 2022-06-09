86d004d501e7dfb36ce25d4077b8b55a2c9e8535
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-06-09 19:06:07 +0530

    Update binarySearch.js

--- SearchingAlgos/binarySearch.js
+++ SearchingAlgos/binarySearch.js
@@ -1,3 +1,44 @@
+// Iterative approach
+function binarySearch(arr, key) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (arr[mid] === key) {
+            return mid;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1
+        }
+        else {
+            high = mid - 1
+        }
+    }
+    return -1;
+}
+console.log(binarySearch([2, 4, 5, 7, 10, 12], 12));
+
+
+// Recursive Approach
+function binarySearchRecursive(arr, key, start = 0, end = arr.length - 1) {
+    if (start > end) {
+        return -1;
+    }
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === key) {
+        return mid;
+    }
+    else if (arr[mid] < key) {
+        return binarySearchRecursive(arr, key, mid + 1, end);
+    }
+    else {
+        return binarySearchRecursive(arr, key, start, mid - 1);
+    }
+}
+console.log(binarySearchRecursive([2, 4, 5, 7, 10, 12], 12));
+
+
 //Binary search, return index of element
 let array = [1,2,3,4,5,6,7,8,9,10];
 			
