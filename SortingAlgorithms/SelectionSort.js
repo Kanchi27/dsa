4a24168ebdfcd6bf1ec35863a369e5e34ef8c96f
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-06-18 17:49:41 +0530

    Update SelectionSort.js

--- SortingAlgorithms/SelectionSort.js
+++ SortingAlgorithms/SelectionSort.js
@@ -1,6 +1,7 @@
 // Selection Sort -> find min element each time and place it at the top
 // O(n2) - worst case
-// best case - O(n) -> find min element place it at the start (and shift all other elements forward by one step/swap it with the min elem )also keep a track if each prev element is smaller than next element parallel to finding min element
+// best case - O(n) -> find min element place it at the start (and shift all other elements forward by one step/swap 
+// it with the min elem )also keep a track if each prev element is smaller than next element parallel to finding min element
 function swap(a,b,arr){
     return [arr[b], arr[a]] = [arr[a], arr[b]];
   }
