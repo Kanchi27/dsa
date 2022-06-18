5fdeb2ff89da05ea0e47f331c5f066d7f1993d0b
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-06-18 19:57:36 +0530

    Update InsertionSort.js

--- SortingAlgorithms/InsertionSort.js
+++ SortingAlgorithms/InsertionSort.js
@@ -6,13 +6,13 @@
 
 function insertionSort(arr){
     for(let i=1;i<arr.length;i++){
-      let temp = arr[i];
-      let j = i-1;
+      let temp = arr[i];        // i=1,temp = 14
+      let j = i-1;              // j=0,arr[j] = 20
       while(j>=0 && arr[j] > temp){
-        arr[j+1] = arr[j];      // j=0, arr[1] = 20 ; j=1, arr[2]= 20
-        j--;                    // j= -1 ; j=0
+        arr[j+1] = arr[j];      // [20,20,13,15,3]
+        j--;                    // j= -1 
       }
-      arr[j+1] = temp; // arr[0] = 14; arr[1]= 13  ,at the end, j will point to its initial value and restore the temp var
+      arr[j+1] = temp;         // [14,20,13,15,3] at the end, j will point to its initial value and restore the temp var
     }
     return arr
   }
