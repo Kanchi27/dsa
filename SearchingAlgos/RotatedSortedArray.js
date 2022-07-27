d60de22b3166d7328ffdae1e36a19d41ecb2cce2
Kanchi27 <43461269+Kanchi27@users.noreply.github.com>
2022-07-28 00:35:19 +0530

    Update RotatedSortedArray.js

--- SearchingAlgos/RotatedSortedArray.js
+++ SearchingAlgos/RotatedSortedArray.js
@@ -1,3 +1,18 @@
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+
+// Algo : if you are at mid, and  if start<mid => left sorted  else right sorted
+// now target in case of left sorted lies btw start and mid, discard right portion
+// else in right sorted, if target lies between mid and end, discard left portion
+
 function search(arr,target){
   let start = 0;
   let end = arr.length - 1;
